f98b3341331b372ab7835c8699110ea03e06d24e
Darko Gjorgjijoski <darkokic@yahoo.com>
2014-05-10 22:20:32 +0200

    Problem solved.

--- zbirka_1/matrici/zad_111.c
+++ zbirka_1/matrici/zad_111.c
@@ -14,6 +14,38 @@
 
 int main()
 {
+    
+    int i, j, n, sporedna = 0,  glavna = 0;
+    int a[MAX][MAX];
+
+    printf("Vnesi golemina na matricata: ");
+    scanf("%d", &n);
+
+    //citanje
+    printf("Vnesuvaj ja matricata\n");
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    
+    
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+        // sobiranje i sporedna i glavna
+            if(i==j)
+                glavna += a[i][j];
+            if(i+j == n-1)
+                sporedna += a[i][j];
+        }
+    }
+    
+    printf("Zbirot na glavnata e: %d\n", glavna);
+    printf("Zbirot na sporednata e: %d\n", sporedna);
 
     return 0;
 }
