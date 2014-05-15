a2eba96ddc306a78c476445a3441a0ddc82f5d7f
Darko Gjorgjijoski <darkokic@yahoo.com>
2014-05-15 23:41:39 +0200

    Problem solved

--- zbirka_1/matrici/zad_120.c
+++ zbirka_1/matrici/zad_120.c
@@ -15,8 +15,45 @@
 /* 12                                                                         */
 /******************************************************************************/
 
+#include <stdio.h>
+
 int main()
 {
+    int maksi, maksj, i, j, maksEl, m, n;
+    int a[10][10];
+
+    printf("How much rows the 2d array will have: ");
+    scanf("%d", &m);
+    printf("How much columns the 2d array will have: ");
+    scanf("%d", &n);
+
+    printf("Enter the array:\n");
+    for(i = 0; i<m; i++)
+    {
+        for(j = 0; j<n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    maksEl = a[0][0];
+    for(i = 0; i<m; ++i)
+    {
+        for(j = 0; j<n; ++j)
+        {
+            if(a[i][j] > maksEl)
+            {
+                maksEl = a[i][j];
+                maksi = i;
+                maksj = j;
+            }
+        }
+    }
+
+    printf("%d %d\n", maksi+1, maksj+1);
+    printf("%d", maksEl);
+
+
 
     return 0;
 }
