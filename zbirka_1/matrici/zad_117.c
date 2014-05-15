ccd1f84b81562dfe594f4ab2dd609a5064f5b090
Darko Gjorgjijoski <darkokic@yahoo.com>
2014-05-16 00:37:26 +0200

    Problem solved

--- zbirka_1/matrici/zad_117.c
+++ zbirka_1/matrici/zad_117.c
@@ -17,6 +17,41 @@
 
 int main()
 {
+    int i, j, tmp, n;
+    int a[10][10];
+    printf("Vnesi koloni i redici: ");
+    scanf("%d", &n);
+
+    printf("Vnesuvaj ja matricata...\n");
+
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i=0; i<n/2; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            tmp = a[i][j];
+            a[i][j] = a[n-1-i][j];
+            a[n-1-i][j] = tmp;
+        }
+    }
+
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<n; j++)
+        {
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+
 
     return 0;
 }
