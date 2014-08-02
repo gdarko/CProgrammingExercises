baa0c54497a3507b63318ad3299757907851883e
Darko Gjorgjijoski <darkokic@yahoo.com>
2014-08-02 17:16:42 +0200

    Problem Solved

--- zbirka_1/funkcii/zad_127.c
+++ zbirka_1/funkcii/zad_127.c
@@ -9,8 +9,31 @@
 /* 17                                                                         */
 /******************************************************************************/
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int count_digits(int n)
+{
+    if(n==0)
+        return 0;
+    return 1+count_digits(n/10);
+}
+
+int add_digits(int n)
+{
+    if(n==0)
+        return 0;
+    return (n%10)+add_digits(n/10);
+}
+
 int main()
 {
+    int n;
+    scanf("%d", &n);
+
+    printf("Number of digits: %d\n", count_digits(n));
+    printf("Sum the of digits: %d\n", add_digits(n));
 
     return 0;
 }
+
