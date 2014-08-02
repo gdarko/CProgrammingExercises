6bbedd09536d49505d34a77df288d179678b9aea
Darko Gjorgjijoski <darkokic@yahoo.com>
2014-08-02 19:07:33 +0200

    Problem Solved

--- zbirka_1/funkcii/zad_137.c
+++ zbirka_1/funkcii/zad_137.c
@@ -11,8 +11,40 @@
 /* 3                                                                          */
 /******************************************************************************/
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int count_digits(int n)
+{
+    if(n<=0)
+        return 0;
+    return 1+count_digits(n/10);
+}
+
 int main()
 {
+    int evenNum = 0,
+        oddNum = 0,
+        i, n, a[100];
+
+    scanf("%d", &n);
+
+    for(i=0; i<n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    for(i=0; i<n; i++)
+    {
+        if((count_digits(a[i]))%2==0)
+            evenNum++;
+        else
+            oddNum++;
+    }
+
+    printf("Even digits numbers count: %d\n", evenNum);
+    printf("Odd digits numbers count: %d\n", oddNum);
 
     return 0;
 }
+
