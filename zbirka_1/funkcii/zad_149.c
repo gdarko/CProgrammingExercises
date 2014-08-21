92a95d473108a878b2f24a62774de8902ff224be
Darko Gjorgjijoski <darkokic@yahoo.com>
2014-08-21 17:35:24 +0200

    Problem solved

--- zbirka_1/funkcii/zad_149.c
+++ zbirka_1/funkcii/zad_149.c
@@ -11,8 +11,29 @@
 /* 120                                                                        */
 /******************************************************************************/
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int power(int n, int exp)
+{
+    if(exp==0)
+        return 1;
+    return n*power(n, exp-1);
+}
+
+int sum_pow(int n, int exp_n)
+{
+    if(exp_n==0)
+        return 0;
+    return power(n, exp_n)+sum_pow(n, exp_n-1);
+}
+
 int main()
 {
+    int count = 3;
+    int exp = 4;
 
+    printf("%d", sum_pow(3, 4));
+    
     return 0;
 }
