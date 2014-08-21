80f6254b0f38b45d2bfbd371942f8250ed126924
Darko Gjorgjijoski <darkokic@yahoo.com>
2014-08-21 17:16:52 +0200

    Problem solved

--- zbirka_1/funkcii/zad_159.c
+++ zbirka_1/funkcii/zad_159.c
@@ -11,8 +11,30 @@
 /* 225                                                                        */
 /******************************************************************************/
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int power(int n, int exp)
+{
+    if(exp==0)
+        return 1;
+    return n * power(n, exp-1);
+}
+
+int recursive_sum(int n, int exp)
+{
+    if(n==0)
+        return 0;
+    return ((power(n, exp))+recursive_sum(n-1, exp));
+}
+
 int main()
 {
+    int num = 5;
+    int exponent = 3;
+
+    printf("%d", recursive_sum(num, exponent));
 
     return 0;
 }
+
