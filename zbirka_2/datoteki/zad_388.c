e71e75544234411a15aa5ce06fd7682587f840ab
Darko Gjorgjijoski <darkokic@yahoo.com>
2014-08-25 18:00:59 +0200

    Solution Added

--- zbirka_2/datoteki/zad_388.c
+++ zbirka_2/datoteki/zad_388.c
@@ -8,8 +8,33 @@
 /* Пример излез:                                                              */
 /******************************************************************************/
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
+    char c, corrected = 'o';
+    FILE *input, *output;
 
+    if((input=fopen("greski.txt", "r"))==NULL)
+    {
+        printf("I can't open it!");
+        return -1;
+    }
+    if((output=fopen("greski_out.txt", "w"))==NULL)
+    {
+        printf("I can't open it!");
+        return -1;
+    }
+
+    while((c=fgetc(input))!=EOF)
+    {
+        if(c=='0')
+            fputc(corrected, output);
+        else
+            fputc(c, output);
+
+    }
     return 0;
 }
+
